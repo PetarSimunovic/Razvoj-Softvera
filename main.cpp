1639511ad8414c2afeb46daaaf0a17d04e908185
Petar <simunovic567@gmail.com>
2024-02-06 19:39:47 +0100

    Dodao sam dinamicno crtanje spitova.

--- main.cpp
+++ main.cpp
@@ -1,50 +1,99 @@
 #include <SFML/Graphics.hpp>
+#include <list>
+#include <vector>
+#include <string>
 #include "math.h"
 
+// Visina i širina prozora
+int screenWidth = 1000;
+int screenHeight = 800;
+
+// Stvaranje prozora
+sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "Virus Destroyer");
+
+// Definiramo globalnu teksturu jer nam treba u funkicji
+sf::Texture virusTexture;
+
+// Array di se nalaze svi virusi u igrici
+sf::Sprite virusi[5];
+
+// array za level 1 gdje sadrži koji svi virusi u levlu
+// Jedan predstavalja normalog virusa
+int level1[3] = {0,1,1};
+
+int toDraw;
+
+// Funkcija kreira virus
+int createVirus(){
+    for (int i = 1;i < sizeof(level1)/sizeof(int);i++){
+        if (level1[i] == 1){
+            virusi[i] = sf::Sprite(virusTexture);
+            virusi[i].setPosition(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+            toDraw = i;
+        }
+        else {
+            toDraw = 0;
+        }
+    }
+}
+
+// Gornja funkcija krira viruse u "Kompjuteru" a ovo ih crta
+int drawVirus(){
+    int ifDraw = toDraw ;
+    if (ifDraw != 0){
+        window.draw(virusi[toDraw]);
+    }
+    else{
+        return 0;
+    }
+    
+}
 
 int main()
 {
-    // Visina i širina prozora
-    int screenWidth = 1000;
-    int screenHeight = 800;
 
     // Sat za delta time
     sf::Clock deltaClock;
+   
+    // Učitamo teksturu igraca stavimo "setSmooth" na true za anti-aliasing (Josipe guglaj to)
+    sf::Texture igracTekstura;
+    igracTekstura.loadFromFile("igrac.jpg");
+    igracTekstura.setSmooth(true);
 
-    // Renderiranje prozora
-    sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "Window text");
+    // Učitamo teksturu file stavimo 
+    sf::Texture fileTekstura;
+    fileTekstura.loadFromFile("file.JPG");
+    fileTekstura.setSmooth(true);
 
-    // Učitamo privremnu teksturu stavimo "setSmooth" na true za anti-aliasing (Josipe guglaj to)
-    // Čekamo da Martin pošalje teksture
-    sf::Texture privermenaTekstura;
-    privermenaTekstura.loadFromFile("PrivermenaTextura.png");
-    privermenaTekstura.setSmooth(true);
 
     // S ovime centriramo kameru na igraća
     sf::View center;
     
     // Novi sprite igrac ako ti Mihelec zatreba negdje drugdje u programu samo mo mi reci da ga globaliziram ;) 
     sf::Sprite igrac;
-    igrac.setOrigin(100.f,100.f);
-    igrac.setTexture(privermenaTekstura);
+    igrac.setOrigin(32.f,32.f);
+    igrac.setTexture(igracTekstura);
     igrac.setPosition(screenHeight/2, screenWidth/2);
     igrac.setRotation(0);
 
     // Sprite file to ćemo morati zaštititi od virusa
     sf::Sprite file;
-    file.setTexture(privermenaTekstura);
+    file.setTexture(fileTekstura);
     file.setOrigin(100.f,100.f);
     file.setPosition(screenHeight/2, screenWidth/2);
-    file.setColor(sf::Color::Yellow);   
-
 
     sf::Vector2i pozicijaMisa; // Vektor 2 int
     sf::Vector2f pozicijaIgraca; // Vektor 2 float
     float kut;
     double misX, misB;
 
+    // Ovdje defiiramo stvari za funkciju
+    virusTexture.loadFromFile("virus.jpg");
+    virusTexture.setSmooth(true);
+
     while (window.isOpen())
     {
+        window.clear(sf::Color::Blue);
         sf::Event event;
         
         // Dobivamo vrijeme između slićica po sekundi
@@ -59,11 +108,13 @@ int main()
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
             igrac.move(-500.f * dt,0.f);
-        }
+        }      
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
             igrac.move(500.f * dt,0.f);
         }
-
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            createVirus();
+        }
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -78,7 +129,7 @@ int main()
         igrac.setRotation(angle + 90);
 
         center.setCenter(igrac.getPosition());
-        window.clear(sf::Color::Blue);
+        drawVirus();
         window.setView(center);
         window.draw(file);
         window.draw(igrac);
